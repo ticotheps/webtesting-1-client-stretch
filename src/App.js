b13ed276cd13b631fef28091c3b746577089f5d9
Tico S. Thepsourinthone <ticotheps@gmail.com>
2019-04-08 16:29:27 -0400

    Adds a <header> and a <main> element into the App.js with a new <Items /> component nested inside

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
 import React, { Component } from 'react';
+import { Route } from 'react-router-dom';
+
 import './App.css';
+import Items from './items/Items';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-
+        <header>
+          <h1>Checkout this Cool List of Items!</h1>
+        </header>
+        <main>
+          <Route path="/items" component={Login}></Route>   
+        </main>
       </div>
     );
   }
 }
 
-export default App;
+export default withRouter(App);
